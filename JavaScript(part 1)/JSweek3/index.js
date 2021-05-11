ede9909538634d7fd064e0678afc209b45597000
DariaMalina <Cmoke68blyat>
2021-05-11 18:31:28 +0300

    fix bag(3) for comments of the final task 3 weeks

--- JavaScript(part 1)/JSweek3/index.js
+++ JavaScript(part 1)/JSweek3/index.js
@@ -7,7 +7,7 @@ let daTe;
 let mass;
 let obj = {};
 
-function z(number) {
+function appendZero(number) {
     if (number < 10) {
         return '0' + number
     } else {
@@ -17,76 +17,73 @@ function z(number) {
 
 function doneString(date) {
 
-    return `${date.getFullYear()}-${z(date.getMonth() + 1)}-${z(date.getDate())} ${z(date.getHours())}:${z(date.getMinutes())}`
+    return `${date.getFullYear()}-${appendZero(date.getMonth() + 1)}-${appendZero(date.getDate())} ${appendZero(date.getHours())}:${appendZero(date.getMinutes())}`
 }
 
-function masOk(word, mass) {
+function presenceInArray(word, mass) {
     return mass.includes(word);
 }
-
+function mathOperationAdd(who,daTe,n) {
+    switch (who) {
+        case mass[0] :
+            daTe.setFullYear(daTe.getFullYear() + n);
+            break
+        case mass[1]:
+            daTe.setMonth(daTe.getMonth() + n);
+            break;
+        case mass[2]:
+            daTe.setDate(daTe.getDate() + n)
+            break;
+        case mass[3]:
+            daTe.setHours(daTe.getHours() + n);
+            break;
+        case mass[4]:
+            daTe.setMinutes(daTe.getMinutes() + n);
+            break;
+    }
+}
+function mathOperationSubst(who,daTe,n) {
+    switch (who) {
+        case mass[0] :
+            daTe.setFullYear(daTe.getFullYear() - n);
+            break
+        case mass[1]:
+            daTe.setMonth(daTe.getMonth() - n);
+            break;
+        case mass[2]:
+            daTe.setDate(daTe.getDate() - n)
+            break;
+        case mass[3]:
+            daTe.setHours(daTe.getHours() - n);
+            break;
+        case mass[4]:
+            daTe.setMinutes(daTe.getMinutes() - n);
+            break;
+    }
+}
 module.exports = function (date) {
-
     datet = date.match(/\d+/g);
     daTe = new Date(datet[0], datet[1] - 1, datet[2], datet[3], datet[4]);
     mass = ['years', 'months', 'days', 'hours', 'minutes'];
-
     obj = {
         get value() {
             return doneString(daTe)
         },
         add: function (n, who) {
             for (let i = 0; i < datet.length; i++) {
-
-                if (n > 0 && masOk(who, mass)) {
-                    switch (who) {
-                        case mass[0] :
-                            daTe.setFullYear(daTe.getFullYear() + n);
-                            break
-                        case mass[1]:
-                            daTe.setMonth(daTe.getMonth() + n);
-                            break;
-                        case mass[2]:
-                            daTe.setDate(daTe.getDate() + n)
-                            break;
-                        case mass[3]:
-                            daTe.setHours(daTe.getHours() + n);
-                            break;
-                        case mass[4]:
-                            daTe.setMinutes(daTe.getMinutes() + n);
-                            break;
-                    }
+                if (n > 0 && presenceInArray(who, mass)) {
+                    mathOperationAdd(who,daTe,n);
                     return this
                 }
-                throw new TypeError()
-            }
+            }throw new TypeError()
         },
         subtract: function (n, who) {
-
             for (let i = 0; i < datet.length; i++) {
-
-                if (n > 0 && masOk(who, mass)) {
-
-                    switch (who) {
-                        case mass[0] :
-                            daTe.setFullYear(daTe.getFullYear() - n);
-                            break
-                        case mass[1]:
-                            daTe.setMonth(daTe.getMonth() - n);
-                            break;
-                        case mass[2]:
-                            daTe.setDate(daTe.getDate() - n)
-                            break;
-                        case mass[3]:
-                            daTe.setHours(daTe.getHours() - n);
-                            break;
-                        case mass[4]:
-                            daTe.setMinutes(daTe.getMinutes() - n);
-                            break;
-                    }
+                if (n > 0 && presenceInArray(who, mass)) {
+                    mathOperationSubst(who,daTe,n)
                     return this
                 }
-                throw new TypeError()
-            }
+            }throw new TypeError()
         }
     }
     return obj;
