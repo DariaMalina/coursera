319afcf6ab1b926f8a90248d9ae13d5b1eff50e9
DariaMalina <Cmoke68blyat>
2021-05-11 18:14:23 +0300

    fix bag(2) for comments of the final task 3 weeks

--- JavaScript(part 1)/JSweek3/index.js
+++ JavaScript(part 1)/JSweek3/index.js
@@ -6,27 +6,24 @@ let datet;
 let daTe;
 let mass;
 let obj = {};
+
 function z(number) {
-    if (number<10){
-        return '0'+number
-    } else {return number }
+    if (number < 10) {
+        return '0' + number
+    } else {
+        return number
+    }
 }
+
 function doneString(date) {
-    let done='';
-    done+=date.getFullYear();
-    done+='-'
-    done+=z(date.getMonth() + 1);
-    done+='-'
-    done+=z(date.getDate());
-    done+=' '
-    done+=z(date.getHours());
-    done+=':'
-    done+=z(date.getMinutes());
-    return done
+
+    return `${date.getFullYear()}-${z(date.getMonth() + 1)}-${z(date.getDate())} ${z(date.getHours())}:${z(date.getMinutes())}`
 }
-function masOk(word, mass){
-    return mass.indexOf(word) !== -1;
+
+function masOk(word, mass) {
+    return mass.includes(word);
 }
+
 module.exports = function (date) {
 
     datet = date.match(/\d+/g);
@@ -39,60 +36,54 @@ module.exports = function (date) {
         },
         add: function (n, who) {
             for (let i = 0; i < datet.length; i++) {
-                for (let a = 0; a < mass.length; a++) {
-                    if (n > 0 && masOk(who, mass)) {
-                        switch (who) {
-                            case mass[0] :
-                                 daTe.setFullYear(daTe.getFullYear() + n);
-                                break
-                            case mass[1]:
-                                daTe.setMonth(daTe.getMonth() + n);
-                                break;
-                            case mass[2]:
-                                daTe.setDate(daTe.getDate() + n)
-                                break;
-                            case mass[3]:
-                                daTe.setHours(daTe.getHours() + n);
-                                break;
-                            case mass[4]:
-                                daTe.setMinutes(daTe.getMinutes() + n);
-                                break;
-                        }
 
-                        return this
+                if (n > 0 && masOk(who, mass)) {
+                    switch (who) {
+                        case mass[0] :
+                            daTe.setFullYear(daTe.getFullYear() + n);
+                            break
+                        case mass[1]:
+                            daTe.setMonth(daTe.getMonth() + n);
+                            break;
+                        case mass[2]:
+                            daTe.setDate(daTe.getDate() + n)
+                            break;
+                        case mass[3]:
+                            daTe.setHours(daTe.getHours() + n);
+                            break;
+                        case mass[4]:
+                            daTe.setMinutes(daTe.getMinutes() + n);
+                            break;
                     }
+                    return this
                 }
                 throw new TypeError()
             }
-
         },
         subtract: function (n, who) {
 
             for (let i = 0; i < datet.length; i++) {
-                for (let a = 0; a < mass.length; a++) {
-                    if (n > 0 && masOk(who, mass)) {
 
-                        switch (who) {
-                            case mass[0] :
-                                daTe.setFullYear(daTe.getFullYear() - n);
-                                break
-                            case mass[1]:
-                                daTe.setMonth(daTe.getMonth() - n);
-                                break;
-                            case mass[2]:
-                                daTe.setDate(daTe.getDate() - n)
-                                break;
-                            case mass[3]:
-                                daTe.setHours(daTe.getHours() - n);
-                                break;
-                            case mass[4]:
-                                daTe.setMinutes(daTe.getMinutes() - n);
-                                break;
-                        }
+                if (n > 0 && masOk(who, mass)) {
 
-
-                        return this
+                    switch (who) {
+                        case mass[0] :
+                            daTe.setFullYear(daTe.getFullYear() - n);
+                            break
+                        case mass[1]:
+                            daTe.setMonth(daTe.getMonth() - n);
+                            break;
+                        case mass[2]:
+                            daTe.setDate(daTe.getDate() - n)
+                            break;
+                        case mass[3]:
+                            daTe.setHours(daTe.getHours() - n);
+                            break;
+                        case mass[4]:
+                            daTe.setMinutes(daTe.getMinutes() - n);
+                            break;
                     }
+                    return this
                 }
                 throw new TypeError()
             }
