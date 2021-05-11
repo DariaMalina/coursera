61f1af994a327a18641c03774a1b67c77355383c
DariaMalina <Cmoke68blyat>
2021-05-11 15:26:28 +0300

    fix bag for comments of the final task 3 weeks

--- JavaScript(part 1)/JSweek3/index.js
+++ JavaScript(part 1)/JSweek3/index.js
@@ -6,45 +6,59 @@ let datet;
 let daTe;
 let mass;
 let obj = {};
+function z(number) {
+    if (number<10){
+        return '0'+number
+    } else {return number }
+}
+function doneString(date) {
+    let done='';
+    done+=date.getFullYear();
+    done+='-'
+    done+=z(date.getMonth() + 1);
+    done+='-'
+    done+=z(date.getDate());
+    done+=' '
+    done+=z(date.getHours());
+    done+=':'
+    done+=z(date.getMinutes());
+    return done
+}
+function masOk(word, mass){
+    return mass.indexOf(word) !== -1;
+}
 module.exports = function (date) {
+
     datet = date.match(/\d+/g);
-    daTe = new Date(datet[0], datet[1]-1, datet[2], datet[3], datet[4]);
+    daTe = new Date(datet[0], datet[1] - 1, datet[2], datet[3], datet[4]);
     mass = ['years', 'months', 'days', 'hours', 'minutes'];
+
     obj = {
-        value: `${daTe.getFullYear()}-${daTe.getMonth()}-${daTe.getDate()} ${daTe.getHours()}:${daTe.getMinutes()}`,
+        get value() {
+            return doneString(daTe)
+        },
         add: function (n, who) {
             for (let i = 0; i < datet.length; i++) {
                 for (let a = 0; a < mass.length; a++) {
-                    if (n > 0 && who === mass[a]) {
-                        if (who === mass[0]) {
-                            daTe.setFullYear(daTe.getFullYear() + n)
-                        } else if (who === mass[1]) {
-                            daTe.setMonth(daTe.getMonth() + n)
-                        } else if (who === mass[2]) {
-                            daTe.setDate(daTe.getDate() + n)
-                        } else if (who === mass[3]) {
-                            daTe.setHours(daTe.getHours() + n);
-                        } else if (who === mass[4]) {
-                            daTe.setMinutes(daTe.getMinutes() + n)
+                    if (n > 0 && masOk(who, mass)) {
+                        switch (who) {
+                            case mass[0] :
+                                 daTe.setFullYear(daTe.getFullYear() + n);
+                                break
+                            case mass[1]:
+                                daTe.setMonth(daTe.getMonth() + n);
+                                break;
+                            case mass[2]:
+                                daTe.setDate(daTe.getDate() + n)
+                                break;
+                            case mass[3]:
+                                daTe.setHours(daTe.getHours() + n);
+                                break;
+                            case mass[4]:
+                                daTe.setMinutes(daTe.getMinutes() + n);
+                                break;
                         }
 
-                        let months = daTe.getMonth()+1;
-                        let days = daTe.getDate();
-                        let hours = daTe.getHours();
-                        let minutes = daTe.getMinutes();
-                        if (daTe.getMonth() < 10) {
-                            months = '0' + months;
-                        }
-                        if (daTe.getDate() < 10) {
-                            days = '0' + days;
-                        }
-                        if (daTe.getHours() < 10) {
-                            hours = '0' + hours
-                        }
-                        if (daTe.getMinutes() < 10) {
-                            minutes = '0' + minutes
-                        }
-                        obj.value = `${daTe.getFullYear()}-${months}-${days} ${hours}:${minutes}`
                         return this
                     }
                 }
@@ -56,36 +70,27 @@ module.exports = function (date) {
 
             for (let i = 0; i < datet.length; i++) {
                 for (let a = 0; a < mass.length; a++) {
-                    if (n > 0 && who === mass[a]) {
+                    if (n > 0 && masOk(who, mass)) {
 
-                        if (who === mass[0]) {
-                            daTe.setFullYear(daTe.getFullYear() - n);
-                        } else if (who === mass[1]) {
-                            daTe.setMonth(daTe.getMonth() - n)
-                        } else if (who === mass[2]) {
-                            daTe.setDate(daTe.getDate() - n)
-                        } else if (who === mass[3]) {
-                            daTe.setHours(daTe.getHours() - n)
-                        } else if (who === mass[4]) {
-                            daTe.setMinutes(daTe.getMinutes() - n)
+                        switch (who) {
+                            case mass[0] :
+                                daTe.setFullYear(daTe.getFullYear() - n);
+                                break
+                            case mass[1]:
+                                daTe.setMonth(daTe.getMonth() - n);
+                                break;
+                            case mass[2]:
+                                daTe.setDate(daTe.getDate() - n)
+                                break;
+                            case mass[3]:
+                                daTe.setHours(daTe.getHours() - n);
+                                break;
+                            case mass[4]:
+                                daTe.setMinutes(daTe.getMinutes() - n);
+                                break;
                         }
-                        let months = daTe.getMonth()+1;
-                        let days = daTe.getDate();
-                        let hours = daTe.getHours();
-                        let minutes = daTe.getMinutes();
-                        if (daTe.getMonth() < 10) {
-                            months = '0' + months;
-                        }
-                        if (daTe.getDate() < 10) {
-                            days = '0' + days;
-                        }
-                        if (daTe.getHours() < 10) {
-                            hours = '0' + hours
-                        }
-                        if (daTe.getMinutes() < 10) {
-                            minutes = '0' + minutes
-                        }
-                        obj.value = `${daTe.getFullYear()}-${months}-${days} ${hours}:${minutes}`
+
+
                         return this
                     }
                 }
