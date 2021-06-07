bcbedce89b58a7e7502d3e504d895606ac64f96c
DariaMalina <Cmoke68blyat>
2021-06-07 20:47:39 +0300

    init commit

--- JavaScript(part 1)/JSweek5/index.js
+++ JavaScript(part 1)/JSweek5/index.js
@@ -1,12 +1,28 @@
 module.exports = {
-
+    eventList: {},
     /**
      * @param {String} event
      * @param {Object} subscriber
      * @param {Function} handler
      */
     on: function (event, subscriber, handler) {
-
+        if (subscriber === undefined) {
+            return this
+        } else {
+            if (!this.eventList.hasOwnProperty(event)) {
+                this.eventList[event] = [{
+                    subscriber: subscriber,
+                    handler: handler.bind(subscriber)
+                }]
+                return this
+            } else {
+                this.eventList[event].push({
+                    subscriber: subscriber,
+                    handler: handler.bind(subscriber)
+                })
+                return this
+            }
+        }
     },
 
     /**
@@ -14,13 +30,25 @@ module.exports = {
      * @param {Object} subscriber
      */
     off: function (event, subscriber) {
-
+        if (this.eventList.hasOwnProperty(event)) {
+            let mass = this.eventList[event]
+            this.eventList[event] = mass.filter(el => el.subscriber !== subscriber)
+            return this
+        }
+        return this
     },
 
     /**
      * @param {String} event
      */
     emit: function (event) {
-
+        if (this.eventList.hasOwnProperty(event)) {
+            let mass = this.eventList[event]
+            for (let i = 0; i < mass.length; i++) {
+                mass[i].handler()
+            }
+            return this
+        }
+        return this
     }
 };
